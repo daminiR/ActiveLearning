aefa8cbbb7223d557dc44bc102cc56932bdcfc46
nhendy <nhendy@purdue.edu>
2019-02-16 18:58:23 -0500

    all

--- M1/correlation.py
+++ M1/correlation.py
@@ -1,1 +1,14 @@
-git
+#!/usr/bin/env python3
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+import torch.optim as lr_scheduler
+from torchvision import models
+
+
+if name == '__main__':
+
+    net = models.resnet18(pretrained=True)
