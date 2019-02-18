ded0a5fd01ebce5552b472c370fa168f83396eeb
tanliyon <35442777+tanliyon@users.noreply.github.com>
2019-02-18 10:33:19 -0500

    Changed dict key as file name.

--- M2/uncertainty_sampling.py
+++ M2/uncertainty_sampling.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from torchvision import datasets, models, transforms
 import os
 from math import log
-import matplotlib.pyplot as plt
+import re
 
 BATCH_SIZE = 8
 
@@ -19,32 +19,47 @@ transforms = transforms.Compose([
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 path = os.getcwd()
 root = os.path.join(path, 'VOCdevkit\VOC2012')
-data = datasets.ImageFolder(root= root, transform=transforms)
-loader = torch.utils.data.DataLoader(data, batch_size=BATCH_SIZE, shuffle=True)
 
 
-def uncertain_samples(model, loader):
+class ImageFolderWithPath(datasets.ImageFolder):
+    def __getitem__(self, index):
+        return super(ImageFolderWithPath, self).__getitem__(index), self.imgs[index]
+
+
+def uncertain_samples(model):
     uncertainty_dict = {}
-    for idx, (data, label) in enumerate(loader):
-        visualize_image(data[0])
+    data = ImageFolderWithPath(root=root, transform=transforms)
+    loader = torch.utils.data.DataLoader(data, batch_size=BATCH_SIZE, shuffle=True)
+
+    for idx, value in enumerate(loader):
+        (data, label), (path, _) = value
         model.eval()
         data = data.to(device)
         softmax = nn.Softmax(dim=1)
-        visualize_image(data)
 
         outputs = model(data)
         pred = softmax(outputs)
-        uncertainty_dict = update_uncertainty_dict(uncertainty_dict, idx, pred)
-        break
+        uncertainty_dict = update_uncertainty_dict(uncertainty_dict, path, pred)
+        if idx == 2:
+            break
 
     uncertainty_dict = sorted(uncertainty_dict.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+    print(uncertainty_dict)
     return uncertainty_dict
 
 
-def update_uncertainty_dict(uncertainty_dict, idx, pred):
+def extract_path (path, index):
+    regex = r'(?<=JPEGImages\\)(?s)(.*$)'
+    file_name = re.findall(regex, path[index])
+    file_name = ", ".join(file_name)
+    file_name = file_name.split(".")[0]
+    return file_name
+
+
+def update_uncertainty_dict(uncertainty_dict, path, pred):
     uncertainty = entropy(pred)
     for key, value in enumerate(uncertainty):
-        index = str(key + (idx*BATCH_SIZE))
+        index = extract_path(path, key)
         uncertainty_dict[index] = float(value)
     return uncertainty_dict
 
@@ -53,16 +68,10 @@ def entropy(prediction):
     return torch.div(torch.sum(-prediction * torch.log2(prediction), dim=1), log(4,2))
 
 
-def visualize_image(image):
-    plt.imshow(image.permute(2, 1, 0))
-    plt.show()
-
-
 if __name__ == '__main__':
     net = models.resnet18(pretrained=True)
     num_ftrs = net.fc.in_features
     net.fc = nn.Linear(num_ftrs, 4)#todo: make dataset and consider class size right now it doesnt matter
-    uncertain_samples(net.to(device), loader=loader)
-
+    uncertain_samples(net.to(device))
 
 
