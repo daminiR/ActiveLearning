0dc0921f126b28b8562f27ee84dc7f0d1abab5e0
tanliyon <35442777+tanliyon@users.noreply.github.com>
2019-02-18 08:58:45 -0500

    Added uncertainty dictionary

--- M2/uncertainty_sampling.py
+++ M2/uncertainty_sampling.py
@@ -2,18 +2,12 @@ from __future__ import print_function, division
 
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
-import numpy as np
-import torchvision
 from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
-import time
 import os
-import copy
-import random
-from pprint import pprint
 from math import log
+import matplotlib.pyplot as plt
+
+BATCH_SIZE = 8
 
 transforms = transforms.Compose([
         transforms.RandomResizedCrop(224),
@@ -22,29 +16,47 @@ transforms = transforms.Compose([
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 path = os.getcwd()
 root = os.path.join(path, 'VOCdevkit\VOC2012')
 data = datasets.ImageFolder(root= root, transform=transforms)
-loader = torch.utils.data.DataLoader(data, batch_size=4, shuffle=True)
+loader = torch.utils.data.DataLoader(data, batch_size=BATCH_SIZE, shuffle=True)
+
 
 def uncertain_samples(model, loader):
+    uncertainty_dict = {}
     for idx, (data, label) in enumerate(loader):
+        visualize_image(data[0])
         model.eval()
         data = data.to(device)
-        outputs = model(data)
         softmax = nn.Softmax(dim=1)
-        #TODO: do we need an alg for num class proprtion to the choice of uncertain value for i.e 0.5 and below verus 0.3 and below
+        visualize_image(data)
 
+        outputs = model(data)
         pred = softmax(outputs)
-        print(pred)
-        uncertainty = entropy(pred)
-        print(uncertainty)
+        uncertainty_dict = update_uncertainty_dict(uncertainty_dict, idx, pred)
         break
 
+    uncertainty_dict = sorted(uncertainty_dict.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+    return uncertainty_dict
+
+
+def update_uncertainty_dict(uncertainty_dict, idx, pred):
+    uncertainty = entropy(pred)
+    for key, value in enumerate(uncertainty):
+        index = str(key + (idx*BATCH_SIZE))
+        uncertainty_dict[index] = float(value)
+    return uncertainty_dict
+
+
 def entropy(prediction):
-    return torch.div(torch.sum(-prediction * torch.log2(prediction),dim=1), log(4,2))
+    return torch.div(torch.sum(-prediction * torch.log2(prediction), dim=1), log(4,2))
+
+
+def visualize_image(image):
+    plt.imshow(image.permute(2, 1, 0))
+    plt.show()
+
 
 if __name__ == '__main__':
     net = models.resnet18(pretrained=True)
